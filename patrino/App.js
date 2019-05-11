085101d699925801c4dbaed537650e6e80992a34
Eduardo <edusvieirap@gmail.com>
2019-05-10 23:24:58 -0300

    .

--- patrino/App.js
+++ patrino/App.js
@@ -1,33 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-type Props = {};
-export default class App extends Component<Props> {
+import Login from "./src/components/Login";
+import HomeScreen from "./src/components/HomeScreen";
+
+class Home extends Component<{}> {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "white",
+      elevation: null
+    },
+    header: null
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <StatusBar barStyle="light-content" backgroundColor="#D95D39" />
+        <Login navigation={this.props.navigation} />
       </View>
     );
   }
 }
+
+const App = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "Home"
+    }
+  },
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+        title: "HomeScreen"
+      },
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      title: "Login"
+    }
+  }
+});
+
+export default createAppContainer(App);
 
 const styles = StyleSheet.create({
   container: {
